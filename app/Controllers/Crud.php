53fea1376de3a7f7f1d41ba8dd69bbdc4f3fe1a5
MarcoPilar <marcopilar908@gmail.com>
2021-05-15 13:15:43 -0500

    Ver. 1. Inserción de datos

---
+++ app/Controllers/Crud.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers;
+
+use App\models\CrudModel;
+
+class Crud extends BaseController
+{
+	public function index()
+	{
+		$Crud = new CrudModel();
+		$datos = $Crud->listarNombres();
+
+		$mensaje = session('mensaje');
+
+		$data = [
+			"datos"=> $datos,
+			"mensaje"=> $mensaje
+		];
+
+		return view('listado',$data);
+	}
+	public function crear(){
+		$datos = [
+			"nombre" => $_POST['nombre'],
+			"paterno" => $_POST['paterno'],
+			"materno" => $_POST['materno']
+		];
+		$Crud = new CrudModel();
+		$respuesta = $Crud->insertar($datos);
+		if($respuesta > 0){
+			return redirect()->to(base_url().'/')->with('mensaje','1');
+		}else{
+			return redirect()->to(base_url().'/')->with('mensaje','0');
+		}
+	}
+	public function actualizar(){
+
+	}
+	public function obtenerNombre($idNombre){
+
+	}
+	public function eliminar($idNombre){
+
+	}
+}
---
+++ app/Models/CrudModel.php
@@ -0,0 +1,14 @@
+<?php namespace App\Models;
+    use CodeIgniter\Model;
+        class CrudModel extends Model{
+            public function listarNombres(){
+                $Nombres = $this->db->query("SELECT * FROM t_personas");
+                return $Nombres->getResult();
+            }
+        public function insertar($datos){
+            $Nombres = $this->db->table('t_personas');
+            $Nombres->insert($datos);
+
+            return $this->db->insertID();
+        }
+        }
